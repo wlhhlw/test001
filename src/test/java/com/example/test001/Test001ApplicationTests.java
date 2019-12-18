9e2745908264166dd2628d95ad611df3569766bb
darian.wang <darian.wang@gymboglobal.com>
2019-12-18 16:13:59 +0800

    first commit

---
+++ src/test/java/com/example/test001/Test001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
